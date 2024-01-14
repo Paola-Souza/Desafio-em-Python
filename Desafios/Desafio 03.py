6216e9fc464a5cf554eb592209a462a5379a31fb
Paola de Souza Cipriano <paola.souza.cipriano@gmail.com>
2024-01-14 19:55:44 -0300

    Adicionar primeiros projetos em python

---
+++ Desafios/Desafio 03.py
@@ -0,0 +1,8 @@
+print('########## DESAFIO 03 ##########')
+numero01=input ('Digite o primeiro numero: ')
+numero02=input ('Digite o segundo numero: ')
+soma = int(numero01) + int(numero02)
+print('A soma é: ',soma)
+
+
+
